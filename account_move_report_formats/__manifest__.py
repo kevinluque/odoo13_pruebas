f7fce4dd7843b6f47efd43541b3cce86a8e38833
kevinluque <54952077+kevinluque@users.noreply.github.com>
2022-03-10 13:20:32 -0500

    Add files via upload

--- account_move_report_formats/__manifest__.py
+++ account_move_report_formats/__manifest__.py
@@ -3,7 +3,7 @@
 	'name': 'Factura nuevo formato',
 	'category': 'account',
 	'author': 'ITGRUPO',
-	'depends': ['account','base'],
+	'depends': ['account','base','sale','sale_stock'],
 	'version': '1.0',
 	'description':"""
 		Creacion de formato informe factura
