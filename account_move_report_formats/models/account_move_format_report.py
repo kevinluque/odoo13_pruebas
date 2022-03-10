c091257c6b939e3d57cb6db1d84c7d5ae9be3c60
kevinluque <54952077+kevinluque@users.noreply.github.com>
2022-03-10 16:52:50 -0500

    Add files via upload

--- account_move_report_formats/models/account_move_format_report.py
+++ account_move_report_formats/models/account_move_format_report.py
@@ -7,7 +7,8 @@ from odoo import tools
 
 class Accountove(models.Model):
     _inherit = "account.move"
-    amount_total_to_text = fields.Char(string='Total a texto', compute='get_amount_total_to_text')
+    amount_total_to_text = fields.Char(string='Total a texto', compute='get_amount_total_to_text', store=False)
+    distribucion_impuestos_txt = fields.Text('impuestos',compute="get_distribucion_impuestos_txt",store=False)
     
     def get_amount_total_to_text(self):
         for i in self:
@@ -17,7 +18,22 @@ class Accountove(models.Model):
                 i.amount_total_to_text = ""
                 
                 
-                
+    def get_distribucion_impuestos_txt(self):
+        contenedor = {}
+        contenedor_final = []
+        for i in self:
+            if i.invoice_line_ids:
+                for linea in i.invoice_line_ids:
+                    if linea.tax_ids:
+                        for taxes in linea.tax_ids:
+                            if taxes.name in contenedor:
+                                contenedor[taxes.name]  += (linea.price_tax)
+                            else:
+                                contenedor[taxes.name]  = (linea.price_tax)
+        for z in contenedor.keys():
+            contenedor_final.append([z,("%.2f"%contenedor[z])])
+        self.distribucion_impuestos_txt = str(contenedor_final)
+        return contenedor_final
                 
                 
                 
