d1d3321f9001236e91a03137bcb6c6896c764125
kevinluque <54952077+kevinluque@users.noreply.github.com>
2022-03-10 09:39:52 -0500

    Add files via upload

---
+++ purchase_order_presupuesto_report_formats/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Compra reporte solicitud presupuesto',
+	'category': 'purchase',
+	'author': 'ITGRUPO',
+	'depends': ['purchase'],
+	'version': '1.0',
+	'description':"""
+		Creacion de formato informe solicitud de presupuesto
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+		'views/purchase_order_presupuesto_report.xml',
+	],
+	'installable': True
+}
